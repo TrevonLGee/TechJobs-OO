e70c3fbd91eeca919393b41fc340917759d237ed
TrevonLGee <trevon.gee@gmail.com>
2021-02-14 14:28:03 -0600

    Finished Assignment

---
+++ src/TechJobs/Controllers/JobController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechJobs.Data;
+using TechJobs.ViewModels;
+using TechJobs.Models;
+
+namespace TechJobs.Controllers
+{
+    public class JobController : Controller
+    {
+
+        // Our reference to the data store
+        private static JobData jobData;
+
+        static JobController()
+        {
+            jobData = JobData.GetInstance();
+        }
+
+        // The detail display for a given Job at URLs like /Job?id=17
+        public IActionResult Index(int id)
+        {
+            Job jobId = new Job();
+            jobId = jobData.Find(id);
+
+            return View(jobId);
+        }
+
+        public IActionResult New()
+        {
+            NewJobViewModel newJobViewModel = new NewJobViewModel();
+            return View(newJobViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult New(NewJobViewModel newJobViewModel)
+        {
+
+            if (ModelState.IsValid)
+            {
+                Job newJob = new Job
+                {
+                    Name = newJobViewModel.Name,
+                    Employer = jobData.Employers.Find(newJobViewModel.EmployerID),
+                    Location = jobData.Locations.Find(newJobViewModel.LocationID),
+                    CoreCompetency = jobData.CoreCompetencies.Find(newJobViewModel.CoreCompetencyID),
+                    PositionType = jobData.PositionTypes.Find(newJobViewModel.PositionTypeID)
+                };
+
+                jobData.Jobs.Add(newJob);
+                return Redirect(string.Format("/job?id={0}", newJob.ID));
+            }
+
+            return View(newJobViewModel);
+        }
+    }
+}
